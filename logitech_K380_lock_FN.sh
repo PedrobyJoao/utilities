d2af7458d682146a5cf2ed63083627f752cad180
JPexplorer <jpexplorer@pop-os.localdomain>
2023-03-27 11:24:05 -0300

    feat: logitech_K380_lock_FN.sh

---
+++ logitech_K380_lock_FN.sh
@@ -0,0 +1,1 @@
+sudo echo 'ACTION=="add", SUBSYSTEM=="hidraw", KERNEL=="hidraw*", SUBSYSTEMS=="hid", KERNELS=="*:046D:B342.*", RUN+="/bin/bash -c \"echo -ne '"'"'\x10\xff\x0b\x1e\x00\x00\x00'"'"' > /dev/%k\""' | sudo tee -a /etc/udev/rules.d/70-logi-k380.rules
